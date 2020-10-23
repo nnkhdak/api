68bc73c716869b45b211bc11cbdfcf4b52ac6b4b
pun-pun-pun <nnkhdak@yahoo.co.jp>
2020-10-24 00:20:12 +0900

    commit

--- html/pdo.php
+++ html/pdo.php
@@ -7,7 +7,7 @@ $dbname = 'eikendb';
 $username = 'eikenuser';
 $passwd = 'hife84ty';
 
-$dsn = DaoUtils::toDSN($endpoint,$port,$dbname);
+$dsn = DaoUtils::toDSN($endpoint, $port, $dbname);
 $conn = DaoUtils::connect($dsn, $username, $passwd);
 
 $sql = 'SELECT count(1) as cnt FROM T_ACCOUNT WHERE stepid = "nona"';
